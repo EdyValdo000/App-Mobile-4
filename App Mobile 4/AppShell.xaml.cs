af24184a17a2b15c99130a9dc9f4295c26267ce7
EdyValdo000 <edivaldocoelho000@example.com>
2024-11-20 22:50:25 +0100

    Adicionar arquivos de projeto.

---
+++ App Mobile 4/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace App_Mobile_4
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ App Mobile 4/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace App_Mobile_4
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLampControlClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnDisconnectClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnConnectClicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+/* Alteração não mesclada do projeto 'App Mobile 4 (net8.0-maccatalyst)'
+Antes:
+        private void OnLampTapped(object sender, EventArgs e)
+        {
+Após:
+        private void OnLampTappedAsync(object sender, EventArgs e)
+        {
+*/
+     
+        private async void OnLampTapped(object sender, EventArgs e)
+        {
+            // Identifica qual lâmpada foi tocada
+            var tappedCell = sender as ViewCell;
+            var label = tappedCell.View as StackLayout;
+            var lampLabel = label.Children[0] as Label;  // O nome da lâmpada é o primeiro Label
+
+            // Exibe o alerta com o nome da lâmpada clicada
+            await DisplayAlert("Lâmpada Selecionada", $"Você clicou na {lampLabel.Text}", "OK");
+
+        }
+    }
+
+}
